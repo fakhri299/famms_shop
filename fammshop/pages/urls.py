96c2c73f9e433ddcd8301605b8561a8a3883a5d6
Fakhri <mmmdov.fxri@bk.ru>
2022-10-07 10:48:15 +0400

    s was removed

--- fammshop/pages/urls.py
+++ fammshop/pages/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='home'),
-    path('testimonials',views.testimonial,name='testimonial'),
+    path('testimonial',views.testimonial,name='testimonial'),
     path('about',views.about,name='about'),
     path('contact',views.ContactView.as_view(),name='contact'),
     path('search/',views.search,name='search'),
